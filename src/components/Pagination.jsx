481c6724bcbb8431be3020b22ffac3bcb6378edd
Michael Veselov <michael.veselov@gmail.com>
2023-03-24 22:57:16 +0300

    change styles and import of lodash

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import _ from 'lodash';
+import { range } from 'lodash';
 
 const Pagination = (props) => {
   const { itemCount, pageSize, currentPage, onPageChange } = props;
@@ -8,21 +8,21 @@ const Pagination = (props) => {
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount === 1) return null;
 
-  const pages = _.range(1, pageCount + 1);
+  const pages = range(1, pageCount + 1);
 
   return (
     <nav>
       <ul className='pagination justify-content-center'>
-        {pages.map((page) => (
-          <li
-            className={'page-item' + (page === currentPage ? ' active' : '')}
-            key={'page_' + page}
-          >
-            <button className='page-link' onClick={() => onPageChange(page)}>
-              {page}
-            </button>
-          </li>
-        ))}
+        {pages.map((page) => {
+          const activeClass = page === currentPage ? ' active' : '';
+          return (
+            <li className={`page-item ${activeClass}`} key={`page_${page}`}>
+              <button className='page-link' onClick={() => onPageChange(page)}>
+                {page}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
