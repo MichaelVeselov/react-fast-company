53f514a01edc7d2a778acfdb1199f5590d18e21d
Michael Veselov <michael.veselov@gmail.com>
2023-04-24 16:38:28 +0300

    added login form and validation

---
+++ src/components/TextField.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+import PropTypes from 'prop-types';
+
+const TextField = (props) => {
+  const { label, type, name, value, error, onChange } = props;
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  return (
+    <div className='mb-4'>
+      <label htmlFor={name}>{label}</label>
+      <div className='input-group has-validation'>
+        <input
+          className={error ? 'form-control is-invalid' : 'form-control'}
+          type={showPassword ? 'text' : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+        {type === 'password' && (
+          <button
+            className='btn btn-outline-primary'
+            type='button'
+            onClick={toggleShowPassword}
+          >
+            <i className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i>
+          </button>
+        )}
+        {error && <div className='invalid-feedback'>{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+TextField.defaultProps = {
+  type: 'text',
+};
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default TextField;
